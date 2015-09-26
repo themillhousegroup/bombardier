ef482d1c2f049175e7fda4ca4b400f2a0417bfff
John Marshall <john@themillhousegroup.com>
2015-09-26 21:19:07 +1000

    Companion object that reads the BOM-sourced data file

--- src/main/scala/com/themillhousegroup/bombardier/WeatherStation.scala
+++ src/main/scala/com/themillhousegroup/bombardier/WeatherStation.scala
@@ -1,5 +1,15 @@
 package com.themillhousegroup.bombardier
 
+object WeatherStation {
+  val weatherStationFileName = "IDY2126.dat"
+  private val weatherStationSource = scala.io.Source.fromFile(weatherStationFileName)
+  private val allStations = weatherStationSource.getLines.drop(1).map(stringToWeatherStation)
+
+  def stringToWeatherStation(s:String):WeatherStation = {
+    WeatherStation(1, "name", "code", 1D, 2D, "VIC")
+  }
+}
+
 /**
  * Represents a BOM weather station that can
  * supply weather observation data.
