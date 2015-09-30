d931b4722ddde5cdab02e6f132ba388d8ddb79f0
John Marshall <john@themillhousegroup.com>
2015-09-30 10:38:32 +1000

    Case class representing an individual weather observation

---
+++ src/main/scala/com/themillhousegroup/bombardier/Observation.scala
@@ -0,0 +1,21 @@
+package com.themillhousegroup.bombardier
+
+import java.io.File
+
+/**
+ * Represents an observation from a BOM weather station
+ */
+case class Observation(
+  dateTimeUtcMillis: Long,
+  apparentTemperature: Double,
+  cloud: String,
+  cloudBaseMetres: Int,
+  cloudOktas: Int,
+  airTemperature: Double,
+  dewPoint: Double,
+  barometricPressure: Double,
+  relativeHumidity: Int,
+  weather: String,
+  windDirection: String,
+  windSpeedKmh: Int,
+  windSpeedKnots: Int)
