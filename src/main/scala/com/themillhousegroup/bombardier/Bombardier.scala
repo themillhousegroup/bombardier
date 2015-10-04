4dd69537a6920f49f5bc7bb8c5b00531c4e0867b
John Marshall <john@themillhousegroup.com>
2015-10-05 10:12:08 +1100

    Iterating on what the main API might look like

--- src/main/scala/com/themillhousegroup/bombardier/Bombardier.scala
+++ src/main/scala/com/themillhousegroup/bombardier/Bombardier.scala
@@ -24,4 +24,21 @@ object Bombardier {
   def observationsFor(station: WeatherStation, dateUtcMillis: Long): Seq[Observation] = {
     null
   }
+
+  /**
+   * Finds weather observations for the given latitude/longitude in the given time window (inclusive).
+   * If there is no observation in that window, the returned Seq will be empty.
+   */
+  def observationsFor(latitude: Double, longitude: Double, startDateUtcMillis: Long, endDateUtcMillis: Long): Seq[Observation] = {
+    val closestStation = WeatherStation.byLatLong(latitude, longitude).head
+    observationsFor(closestStation, startDateUtcMillis, endDateUtcMillis)
+  }
+
+  /**
+   * Finds weather observations for the given WeatherStation in the given time window (inclusive).
+   * If there is no observation in that window, the returned Seq will be empty.
+   */
+  def observationsFor(station: WeatherStation, startDateUtcMillis: Long, endDateUtcMillis: Long): Seq[Observation] = {
+    null
+  }
 }
